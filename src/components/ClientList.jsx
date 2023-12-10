cbf504c6f8618f83efef581f78ee31f642a5eeb3
Omkar <omkarmhatre4@gmail.com>
2023-12-11 01:23:06 +0530

    Client Data

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,11 +1,21 @@
 // ClientList.js
 import { useEffect, useState } from "react";
-import { Container, Table } from "react-bootstrap";
-import { fetchClients } from "../services/ClientServices";
+import { Button, Container, Modal, Table } from "react-bootstrap";
+import { deleteClient, fetchClients } from "../services/ClientServices";
+import { useNavigate } from "react-router-dom";
 
 export function ClientList() {
   const [clients, setClients] = useState([]);
+  const [selectedClient, setSelectedClient] = useState("");
+  const [showDialog, setShowDialog] = useState(false);
+  const navigate = useNavigate();
 
+  const openDialogModal= () =>{
+    setShowDialog(true);
+  }
+  const closeDialogModal= () =>{
+    setShowDialog(false);
+  }
   useEffect(() => {
     populateClientState();
   }, []);
@@ -14,11 +24,22 @@ export function ClientList() {
     try {
       const data = await fetchClients();
       setClients(data.customers);
-      console.log(data.customers); // Log the customers array from the fetched data
     } catch (error) {
       console.log(error);
     }
   }
+
+  const handleClientDelete = async()=>{
+    try {
+        await deleteClient(selectedClient);
+        populateClientState();
+        closeDialogModal();
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+
 
   return (
     <Container>
@@ -27,7 +48,6 @@ export function ClientList() {
           <Table responsive="sm">
             <thead>
               <tr>
-                <th>#</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Date of Birth</th>
@@ -38,16 +58,26 @@ export function ClientList() {
               </tr>
             </thead>
             <tbody>
-              {clients.map((customer, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{customer.name}</td>
-                  <td>{customer.email}</td>
-                  <td>{customer.dob}</td>
-                  <td>{customer.contact}</td>
-                  <td>{customer.address}</td>
-                  <td>{customer.weight}</td>
-                  <td>{customer.plan}</td>
+              {clients.map((c) => (
+                <tr >
+                  
+                  <td>{c.name}</td>
+                  <td>{c.email}</td>
+                  <td>{c.dob}</td>
+                  <td>{c.contact}</td>
+                  <td>{c.address}</td>
+                  <td>{c.weight}</td>
+                  <td>{c.plan}</td>
+                  <td>
+                    
+                <Button variant="danger" onClick={() => {
+                        openDialogModal();
+                        setSelectedClient(c.name);
+                        }}>Delete</Button> 
+                <Button variant="primary" onClick={()=>{
+                         navigate(`/edit/${c.name}`)
+                             }}>Edit</Button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -55,6 +85,23 @@ export function ClientList() {
         ) : (
           <p>No Clients Found...!</p>
         )}
+        <Modal show ={showDialog} onHide = {closeDialogModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Confirmation</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Do ypu really want to delete Client?</Modal.Body>
+            <Modal.Footer>
+                <Button variant = "danger" onClick={()=>{
+                    handleClientDelete();
+                }}>
+                    Yes
+                </Button>
+                <Button variant="success" onClick={closeDialogModal}>
+                        No
+                    </Button>
+            </Modal.Footer>
+        </Modal>
+        
       </div>
     </Container>
   );
--- src/services/ClientServices.js
+++ src/services/ClientServices.js
@@ -10,3 +10,21 @@ export async function fetchClients() {
     console.log(error);
   }
 }
+
+export async function deleteClient(name) {
+  try {
+    console.log("Deleting Name:",name)
+    const response = await axios.deleteOne(`http://127.0.0.1:4600/gymdb/register/${name}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function updateClient(updatedData,name){
+    try {
+      const response = await axios.put(`http://127.0.0.1:4600/gymdb/register/${name}`, updatedData);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+}
